8ca599f4def51443c79dd6e6386ca933375a9379
Ednilson <163150527+Kapu22@users.noreply.github.com>
2024-10-28 21:57:31 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Lógica',
+    'Uma folha diz "oi" para uma tora. Que horas são?',
+    'oito horas kk :)'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasília'
+)
+
+criaCartao(
+    'Charadas',
+    'Eu começo de noite, sempre presente no dia, nunca me canso, mas desapareço no meio da manhã. Quem sou eu?',
+    'a letra "N"!'
+)
+
+criaCartao(
+    'Charadas',
+    'Quanto mais você tira, maior ele fica',
+    'um buraco'
+)
